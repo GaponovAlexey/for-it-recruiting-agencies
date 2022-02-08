97c6505d132abb3aaad2f9cbfba29a2246a81d24
Kekelberi <alexei.gaponov@gmail.com>
2022-02-08 19:28:47 +0300

    save

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,22 +1,34 @@
 import { css } from '@emotion/css'
-import React, { FC } from 'react'
-import { useSelector } from 'react-redux'
+import React, { FC, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUserPhotos } from '../Redux/reducer'
 import { RootState } from '../Redux/store'
 import ImgMediaCard from './ImgMediaCard'
 
 export const Main: FC = (): JSX.Element => {
   const photos = useSelector((state: RootState) => state?.allPhoto?.photos)
+  const dispatch = useDispatch()
 
-  console.log(photos)
+  useEffect(() => {
+    dispatch(fetchUserPhotos())
+  }, [])
 
   return (
     <div
       className={css`
         padding: 10px 20px;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
       `}
     >
-      <div>
-        <ImgMediaCard />
+      <div className={css``}>
+        {photos.map((ph: any) => {
+          return (
+            <div>
+              <ImgMediaCard key={ph.id} {...ph} />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
