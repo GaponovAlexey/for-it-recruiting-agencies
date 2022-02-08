ad0550f338e88ec9da03d3ae86378869fe64c245
Kekelberi <alexei.gaponov@gmail.com>
2022-02-08 15:46:46 +0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      start
+    <div className='App'>
+      return <Button variant='contained'>Hello World</Button>;
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
